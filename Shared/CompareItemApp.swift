dce244e12063c1bed7e7021827a21373663ba469
William Santoso <williamsantoso93@yahoo.com>
2021-04-10 15:11:02 +0700

    Initial Commit

---
+++ Shared/CompareItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompareItemApp.swift
+//  Shared
+//
+//  Created by William Santoso on 10/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CompareItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
